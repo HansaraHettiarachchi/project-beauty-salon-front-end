1b3326b17cfddf8c71edf74d6be830bb999d466f
Hansara <hansarasasanka@gmail.com>
2025-10-26 20:55:49 +0530

    main frame

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,14 @@
+
+export default function Hero() {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <div className="w-[900] bg-blue-700 p-4 rounded">
+                <h1 className="text-center text-5xl text-white">
+                    We are stil working on it...
+                    <br />
+                    Come back later
+                </h1>
+            </div>
+        </div>
+    )
+}
