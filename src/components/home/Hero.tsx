7e760ecb53c3dd7e380346cc9be0da71a812f4b6
Hansara <hansarasasanka@gmail.com>
2025-11-06 21:23:26 +0530

    Fixed Some Issues and and updated the paper cut

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,13 +1,13 @@
 import Ring from './Ring';
 import layer1 from "../../assets/images/layer.png";
-// import bg from "../../assets/images/Salon BG-01.png";
+import bg from "../../assets/images/Salon BG-01.png";
 import AppointmentBtn from './AppointmentBtn';
 
 export default function Hero() {
 
     return (
         <>
-            <div className='grid lg:grid-cols-2 mx-auto  bg-[#f5f5f5] h-screen'>
+            <div className='grid lg:grid-cols-2 mx-auto  bg-[#f5f5f5] h-[80vh]'>
                 <div className='flex justify-center lg:justify-end'>
                     <div className='mt-40 transform text-center lg:text-start '>
                         <h1 className='font-extrabold text-6xl leading-18 scale-y-115'>Experince the <br /> Best Beauty </h1>
@@ -39,8 +39,8 @@ export default function Hero() {
                 <img src={layer1} alt="" className='absolute top-40 lg:right-10 xl:right-30 w-auto lg:h-[400px] xl:h-[650px]' />
 
             </div> */}
-            {/* <div className="bg-bottom absolute bottom-20  w-full h-screen m-0 p-0 pointer-events-none" style={{ backgroundImage: `url(${bg})` }} aria-hidden="true" />
-            <div className='pb-8 bg-white absolute bottom-15  w-full pointer-events-none'></div> */}
+            <div className="bg-bottom absolute bottom-0 w-full h-screen m-0 p-0 pointer-events-none" style={{ backgroundImage: `url(${bg})` }} aria-hidden="true" />
+            {/* <div className='bg-white relative bottom-20 w-full pointer-events-none'></div> */}
         </>
     );
 
