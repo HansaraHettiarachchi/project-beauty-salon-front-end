2034a3d6f1f3b4139ebaac7d2e955e967d9a836d
yasiru <thilakshanayasuru@gmail.com>
2025-11-12 20:38:12 +0530

    i remove all code of About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,13 @@
-import AboutUsAbout from "../components/about/AboutUsAbout";
-import AboutUs from "../components/home/AboutUs";
-import Services from "../components/home/Services";
-import SpaExperience from "../components/home/SpaExperience";
+
 import NavBar from "../components/NavBar/NavBar";
 
-export default function about() {
+export default function About() {
   return (
  <>
             <div className="font-serif">
                 <NavBar />
                 
-                <AboutUsAbout/>
-                <SpaExperience />
-                <AboutUs />
-                <Services/>
+               
 
 
             </div>
